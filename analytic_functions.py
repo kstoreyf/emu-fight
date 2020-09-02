15fa33f44bddbc3c89ed9a01050ae13efcbbd73c
Johannes Heyl <johannesheyl@hotmail.com>
2020-09-02 14:55:20 +0100

    Got rid of weird formatting

--- analytic_functions.py
+++ analytic_functions.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Thu Jul 16 18:06:56 2020@author: johannesheyl"""import numpy as npdef eggbox_function(x, y):    return np.exp((2+np.cos(x/2)*np.cos(y/2))**5)def rosenbruck_function(x):    N = np.asarray(x).shape[0]        output = []    for i in range(N):        output.append((1-x[i]**2) + 100*(x[i+1]-x[i]**2)**2)            return np.sum(output)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 14:54:32 2020
+
+@author: johannesheyl
+"""
+
+import numpy as np
+
+def eggbox_function(x, y):
+    return np.exp((2+np.cos(x/2)*np.cos(y/2))**5)
+
+def rosenbruck_function(x):
+    N = np.asarray(x).shape[0]
+    
+    output = []
+    for i in range(N):
+        output.append((1-x[i]**2) + 100*(x[i+1]-x[i]**2)**2)
+        
+    return np.sum(output)
